b507095ed96017592e67903817cd138508c7ab6f
Eduardo Guzman-Diaz <tloz_tlotr@hotmail.com>
2020-08-02 12:20:39 -0400

    vug fix

--- slots-app/src/components/CalculationEngine.ts
+++ slots-app/src/components/CalculationEngine.ts
@@ -41,15 +41,14 @@ export function didSpinWin(bonus: number = 0) {
 }
 
 export function specialCoinEarned(bumpProb: number = 0) {
-    return true
-    // const result1 = Math.floor(Math.random() * 15)
-    // const result2 = Math.floor(Math.random() * 15)
-    // const result = (result2 === result1)
-    // if (result) {
-    //     return true
-    // } else {
-    //     //generate random number
-    //     const randomNumber = Math.random()
-    //     return randomNumber < bumpProb
-    // }
+    const result1 = Math.floor(Math.random() * 15)
+    const result2 = Math.floor(Math.random() * 15)
+    const result = (result2 === result1)
+    if (result) {
+        return true
+    } else {
+        //generate random number
+        const randomNumber = Math.random()
+        return randomNumber < bumpProb
+    }
 }
